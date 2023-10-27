981364e8b334c5b65d3a95ab400487b828216ff0
schmidtw <Weston_Schmidt@alumni.purdue.edu>
2023-10-27 05:10:42 -0700

    Initial creation.

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/schmidtw/muggo
+
+go 1.21.3
---
+++ main.go
@@ -0,0 +1,10 @@
+// SPDX-FileCopyrightText: 2023 Weston Schmidt <weston_schmidt@alumni.purdue.edu>
+// SPDX-License-Identifier: Apache-2.0
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, world.")
+}
